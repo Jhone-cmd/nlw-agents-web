15e5d944e42d6fe99202a4039d2ab91729dcbdd5
Jhone-cmd <jhone.1941@gmail.com>
2025-07-21 11:40:36 -0300

    chore:
    making the request to the server, placing the data on the screen

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,13 +1,36 @@
-import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query'
+import { Link } from 'react-router-dom'
+
+type GetRoomsApiResponse = {
+  id: string
+  name: string
+}
 
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms')
+
+      const result: GetRoomsApiResponse[] = await response.json()
+
+      return result
+    },
+  })
   return (
     <div>
-      Create Room
-      <br />
+      <div>Create Room</div>
+
+      {isLoading && <p>Carregando...</p>}
+
+      <div>
+        {data?.map((room) => {
+          return <p key={room.id}>{room.name}</p>
+        })}
+      </div>
       <Link className="underline" to="/room">
         acessar sala
       </Link>
     </div>
-  );
+  )
 }
